dc0ca908454e1030e08316540f59982cbd283e5e
sean-ao <sean.augustine-obi@kellogg.northwestern.edu>
2022-04-08 21:20:11 +0000

    h

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,9 @@
 -- Expected result:
 -- 2935
 
-
+SELECT SUM(stats.hits)
+FROM stats 
+INNER JOIN players
+ON stats.player_id = players.id
+WHERE players.last_name = "Bonds"
+AND players.first_name= "Barry";
