1adea5cce7443534622c2a9655679a24b25c7cfc
sean-ao <sean.augustine-obi@kellogg.northwestern.edu>
2022-04-08 21:29:08 +0000

    k

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,11 @@
 -- Drop the students table
+DROP TABLE students;
 
 -- Recreate it
+
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+  email TEXT
+);
